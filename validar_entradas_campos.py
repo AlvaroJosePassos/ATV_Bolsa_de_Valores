fea20c498dc3d5a79ce64bb1d9baf22c16fe457f
AlvaroJosePassos <115745009+AlvaroJosePassos@users.noreply.github.com>
2022-12-01 14:00:04 -0300

    Documentação da validação das entradas

--- validar_entradas_campos.py
+++ validar_entradas_campos.py
@@ -2,6 +2,7 @@ from modulos import *
 
 class validadores:
     def validar_campo(self, info):
+        """--> função que faz a verificação de erro para n ser possível colocar caracteres diferentes de float"""
         if info == '':
             return True
         try:
